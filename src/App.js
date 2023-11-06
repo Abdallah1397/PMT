cf86fae18013242893b66969838c66a60427bd7d
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-11-06 13:37:21 +0200

    Creating the axios interceptors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import LoginSVG from "./assets/svgs/loginSVG/loginSVG";
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
+import AxiosErrorHanlder from "./interceptors/axiosInterceptors";
 import SignIn from "./pages/signIn/signIn";
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       <Routes>
-        <Route path="/login" element={<SignIn/>}/>
+        <Route path="/login" element={<SignIn />} />
       </Routes>
+      {/* Error Handlers */}
+      <AxiosErrorHanlder/>
       {/* <Footer/> */}
     </div>
   );
---
+++ src/interceptors/axiosInterceptors.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { api } from "./axiosInstance";
+import { Snackbar, Alert } from "@mui/material";
+
+const AxiosErrorHanlder = () => {
+  // Error States
+  const [errorStates, setErrorStates] = useState({
+    forbiddenAccess: false,
+    serverErr: false,
+    networkErr: false,
+    errorMsg: "",
+  });
+  //   Destructuring for the errorStates
+  const { forbiddenAccess, serverErr, networkErr, errorMsg } = errorStates;
+  //   Handle Axios Response
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.request.status === 0) {
+        setErrorStates({
+          forbiddenAccess: false,
+          serverErr: false,
+          networkErr: true,
+          errorMsg: "Network Error, Can't connect to host!",
+        });
+      } else if (error.request.status === 403) {
+        setErrorStates({
+          forbiddenAccess: true,
+          serverErr: false,
+          networkErr: false,
+          errorMsg:
+            "Forbidden Access, you are unauthorized to perform this action!",
+        });
+      } else if (error.request.status === 500) {
+        setErrorStates({
+          forbiddenAccess: true,
+          serverErr: false,
+          networkErr: false,
+          timeoutErr: false,
+          validationErr: false,
+          errorMsg:
+            "Interak Server Error, please contact system administrator!",
+        });
+      }
+      throw error; // Propagate the error
+    }
+  );
+  //   Close Snackbar
+  const closeSnackbar = () => {
+    setErrorStates({
+      forbiddenAccess: false,
+      serverErr: false,
+      networkErr: false,
+      errorMsg: "",
+    });
+  };
+  return (
+    <Snackbar
+      open={forbiddenAccess || serverErr || networkErr}
+      anchorOrigin={{ horizontal: "center", vertical: "top" }}
+      autoHideDuration={6000}
+    >
+      <Alert severity="error" variant="filled" onClose={closeSnackbar}>
+        {errorMsg}
+      </Alert>
+    </Snackbar>
+  );
+};
+export default AxiosErrorHanlder;
