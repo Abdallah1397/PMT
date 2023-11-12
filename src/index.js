a99bb3a016eba9a39c8976a4aacfef2f7c16bf14
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-11-12 15:54:41 +0200

    Persist the Redux state

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fontsource/poppins";
 import "./index.css";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+// Root Reducer that contains all reducers
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+// We will use Redux Persist to persist the Redux state across page reloads or app restarts.
+// Redux Persist Configurations
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+// Persisted Reducer :  returns a new reducer that, when used to create the Redux store, will automatically handle persisting and rehydrating the state.
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+// Store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+// Persistor: takes the store and returns persistor obj
+// The persistor object has a purge method that can be used to clear the persisted state
+export const persistor = persistStore(store);
