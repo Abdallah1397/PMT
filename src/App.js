f9c0b7e5d101e14eaabdc48b832603bf87411ce6
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-11-13 10:41:41 +0200

    Control navbar and footer based on isAuth status

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-import LoginSVG from "./assets/svgs/loginSVG/loginSVG";
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
 import AxiosErrorHanlder from "./interceptors/axiosInterceptors";
 import SignIn from "./pages/signIn/signIn";
 function App() {
+  // select the user state
+  const user = useSelector((state) => state.user);
+  const { isAuth} = user;
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      {isAuth && <Navbar />}
       <Routes>
         <Route path="/signin" element={<SignIn />} />
       </Routes>
       {/* Error Handlers */}
-      <AxiosErrorHanlder/>
-      {/* <Footer/> */}
+      <AxiosErrorHanlder />
+      {isAuth && <Footer />}
     </div>
   );
 }
