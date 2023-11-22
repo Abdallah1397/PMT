6e491d74231e03b320510cf8f5ecef9de7e7c00a
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-11-22 13:03:29 +0200

    Create reusable title component

---
+++ src/components/title/title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './title.css';
+const Title = () => {
+  return (
+    <div className='title'>
+        <div className='lineTitle'/>
+        <h1 className='headingTitle'>Profiles</h1>
+        <div className='lineTitle'/>
+    </div>
+  )
+}
+
+export default Title
