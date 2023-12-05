bf2a9d6bae82e5187d41e18ab594ac07e48f3241
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-12-05 12:55:28 +0200

    Enhance the home page view

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Banner from "../../components/banner/bannner";
+import HomeSVG from "../../assets/svgs/homeSVG/homeSVG";
+import Hero from "../../components/hero/hero";
+import AllBenefits from "../../components/allBenefits/allBenefits";
+import AllQuotes from "../../components/allQuotes/allQuotes";
+import Clients from "../../components/clients/clients";
+import "./home.css";
 
+// Home Page 
 const Home = () => {
-  return <div>Home</div>;
+  // Authenticated status
+  const { isAuth } = useSelector((state) => state.user);
+  return (
+    <div className="container-fluid">
+      {/* Home Banner */}
+      <Banner
+        title={"Start The Profiles Management In Several Ways"}
+        subTitle={
+          "Before you start the Profile Management Tool, you need to be aware of the restrictions and ensure that the prerequisites are met."
+        }
+        startedTo={isAuth ? "/profiles" : "/signin"}
+        svgBanner={<HomeSVG />}
+      />
+      {/* Benfefits Section */}
+      <div className="row homeSectionStyle">
+        <AllBenefits />
+      </div>
+      {/* Hero */}
+      <div className="row">
+        <Hero
+          title="Explore the leading user profile management tools in-depth"
+          to="/profiles"
+          buttonText="Discover Now"
+        />
+      </div>
+      {/* Pepole Saying */}
+      <div className="row" style={{ padding: "2rem 2rem 4rem 2rem" }}>
+        <AllQuotes />
+      </div>
+      {/* Clients Slider */}
+      <div className="row homeSectionStyle">
+        <Clients />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
